8716611337c28e007ee9a282b4ec16900cb44f78
li.wei <li.wei01@ikasinfo.com>
2023-10-10 10:06:26 +0800

    fix: 修复ts类型、调整export入参

--- packages/component/src/Form/types.ts
+++ packages/component/src/Form/types.ts
@@ -1,10 +1,10 @@
 import { FormItemProps, FormProps } from 'element-plus'
-import { CSSProperties } from 'vue'
+import { CSSProperties, HTMLAttributes } from 'vue'
 import { ExpandColumn, Column, NonModel } from '../useColumn'
 
 export type FormColumn = ExpandColumn<
   Column,
-  { formItemProps: Partial<FormItemProps> & { class?: string; style?: CSSProperties } }
+  { formItemProps: Partial<FormItemProps> & { class?: HTMLAttributes['class']; style?: CSSProperties } }
 >
 
 export interface OneFormProps extends /* @vue-ignore */ Partial<FormProps> {
