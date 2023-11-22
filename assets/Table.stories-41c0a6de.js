c3fd32a23ec8a53ae170921da5efccf0d57ec915
Fzlw <1416017757@qq.com>
2023-11-22 10:02:43 +0000

    :construction_worker:CI built at 2023-11-22 10:02:43

--- assets/Table.stories-41c0a6de.js
+++ assets/Table.stories-41c0a6de.js
@@ -1,25 +1,28 @@
-import{a as T,b as S,o,n as R}from"./vue.esm-bundler-2b92b106.js";import"./index-c013ead5.js";import{_ as l,L as U}from"./el-message-box-9e66d856.js";import{E as y}from"./el-pagination-6c624e40.js";import{u as s,F as d}from"./index-14fd34f7.js";import"./_commonjsHelpers-725317a4.js";const te={title:"Example/OneTable",component:l,tags:["autodocs"],argTypes:{loading:{description:"组件加载状态 通过vLoading指令实现",defaultValue:{summary:!1},control:{type:"boolean"}},columns:{description:"列信息",defaultValue:{summary:"TableColumn<T>[]"},control:{type:"object"}},selected:{description:"table组件在可选择模式下当前选择的项",defaultValue:{summary:"T[]"},control:{type:"object"}}},parameters:{docs:{description:{component:`完全基于 el-table 组件 除列举的props外 其他$attrs会透传至el-table
+import{a as c,b,o,n as Y}from"./vue.esm-bundler-2b92b106.js";import"./index-c013ead5.js";import{_ as l,L as Z}from"./el-message-box-9e66d856.js";import{E as w}from"./el-pagination-6c624e40.js";import{u as s,F as d}from"./index-c9e6012b.js";import"./_commonjsHelpers-725317a4.js";const se={title:"Example/OneTable",component:l,tags:["autodocs"],argTypes:{loading:{description:"组件加载状态 通过vLoading指令实现",defaultValue:{summary:!1},control:{type:"boolean"}},columns:{description:"列信息",defaultValue:{summary:"TableColumn<T>[]"},control:{type:"object"}},selected:{description:"table组件在可选择模式下当前选择的项",defaultValue:{summary:"T[]"},control:{type:"object"}}},parameters:{docs:{description:{component:`完全基于 el-table 组件 除列举的props外 其他$attrs会透传至el-table
 selection-change事件会失效 取而代之可以直接使用 v-model:selected
-https://element-plus.org/zh-CN/component/table.html`}}}},p={args:{height:300,data:[{date:"2016-05-03",name:"Tom",address:"No. 189, Grove St, Los Angeles"},{date:"2016-05-02",name:"Tom",address:"No. 189, Grove St, Los Angeles"},{date:"2016-05-04",name:"Tom",address:"No. 189, Grove St, Los Angeles"},{date:"2016-05-01",name:"Tom",address:"No. 189, Grove St, Los Angeles"}],columns:[{label:"Date",prop:"date"},{label:"Name",prop:"name"},{label:"Address",prop:"address"}]}},u={render:()=>({components:{Table:l,ElPagination:y},setup(){const{tableState:a,handleQuery:r,pagination:t}=s({columns:[{label:"Date",prop:"date",rFormat:d.dateTime},{label:"Name",prop:"name"},{label:"Address",prop:"address",formatter(e,n,h,F){return T("div",null,[S("addrrrrr")])}}],query(){return new Promise(e=>{setTimeout(()=>{const n=new Array(20).fill({date:new Date().toString(),name:"name",address:Math.random().toString()});e({list:n,total:100})},1e3)})}});return o(r),{tableState:a,pagination:t}},template:`
+https://element-plus.org/zh-CN/component/table.html`}}}},g={args:{height:300,data:[{date:"2016-05-03",name:"Tom",address:"No. 189, Grove St, Los Angeles"},{date:"2016-05-02",name:"Tom",address:"No. 189, Grove St, Los Angeles"},{date:"2016-05-04",name:"Tom",address:"No. 189, Grove St, Los Angeles"},{date:"2016-05-01",name:"Tom",address:"No. 189, Grove St, Los Angeles"}],columns:[{label:"Date",prop:"date"},{label:"Name",prop:"name"},{label:"Address",prop:"address"}]}},m={render:()=>({components:{Table:l,ElPagination:w},setup(){const{tableState:a,handleQuery:r,pagination:t}=s({columns:[{label:"Date",prop:"date",rFormat:d.dateTime},{label:"Name",prop:"name"},{label:"Address",prop:"address",formatter(n,e,u,F){return c("div",null,[b("addrrrrr")])}}],query(){return new Promise(n=>{setTimeout(()=>{const e=new Array(20).fill({date:new Date().toString(),name:"name",address:Math.random().toString()});n({list:e,total:100})},1e3)})}});return o(r),{tableState:a,pagination:t}},template:`
         <Table v-bind="tableState" height=300 />
         <ElPagination v-bind="pagination" />
-      `})},c={render:()=>({components:{Table:l,ElPagination:y},setup(){const{tableState:a,handleQuery:r,pagination:t}=s({columns:[{label:"Date",prop:"date",rFormat:d.dateTime},{label:"Name",prop:"name"},{label:"Address",prop:"address",formatter(e,n,h,F){return T("div",null,[S("addrrrrr")])}}],query(){return new Promise(e=>{setTimeout(()=>{const n=new Array(10).fill({date:new Date().toString(),name:"name",address:Math.random().toString()});e({list:n,total:100})},1e3)})},pagination:{pageSize:10,pageSizes:[10,20]}});return o(r),{tableState:a,pagination:t}},template:`
+      `})},T={render:()=>({components:{Table:l,ElPagination:w},setup(){const{tableState:a,handleQuery:r,pagination:t}=s({columns:[{label:"Date",prop:"date",rFormat:d.dateTime},{label:"Name",prop:"name"},{label:"Address",prop:"address",formatter(n,e,u,F){return c("div",null,[b("addrrrrr")])}}],query(){return new Promise(n=>{setTimeout(()=>{const e=new Array(10).fill({date:new Date().toString(),name:"name",address:Math.random().toString()});n({list:e,total:100})},1e3)})},pagination:{pageSize:10,pageSizes:[10,20]}});return o(r),{tableState:a,pagination:t}},template:`
         <Table v-bind="tableState" height=300 />
         <ElPagination v-bind="pagination" />
-      `})},i={render:()=>({components:{Table:l},setup(){const{tableState:a,handleQuery:r}=s({columns:[{label:"Date",prop:"date",rFormat:d.dateTime},{label:"Name",prop:"name"},{label:"Address",prop:"address",formatter(t,e,n,h){return T("div",null,[S("addrrrrr")])}}],query(){return new Promise(t=>{setTimeout(()=>{const e=new Array(10).fill({date:new Date().toString(),name:"name",address:Math.random().toString()});t({list:e,total:100})},1e3)})},mode:U.infinite});return o(r),{tableState:a}},template:`
+      `})},i={render:()=>({components:{Table:l},setup(){const{tableState:a,handleQuery:r}=s({columns:[{label:"Date",prop:"date",rFormat:d.dateTime},{label:"Name",prop:"name"},{label:"Address",prop:"address",formatter(t,n,e,u){return c("div",null,[b("addrrrrr")])}}],query(){return new Promise(t=>{setTimeout(()=>{const n=new Array(10).fill({date:new Date().toString(),name:"name",address:Math.random().toString()});t({list:n,total:100})},1e3)})},mode:Z.infinite});return o(r),{tableState:a}},template:`
         <Table v-bind="tableState" height=300 />
-      `})},b={render:()=>({components:{Table:l,ElPagination:y},setup(){const{tableState:a,handleQuery:r,pagination:t}=s({columns:[{type:"selection",width:40},{label:"Date",prop:"date",rFormat:d.dateTime},{label:"Name",prop:"name"},{label:"Address",prop:"address"}],query(){return new Promise(e=>{setTimeout(()=>{const n=new Array(10).fill(0).map(()=>({date:new Date().toString(),name:"name",address:Math.random().toString()}));e({list:n,total:100})},1e3)})}});return o(r),{tableState:a,pagination:t}},template:`
+      `})},S={render:()=>({components:{Table:l,ElPagination:w},setup(){const{tableState:a,handleQuery:r,pagination:t}=s({columns:[{type:"selection",width:40},{label:"Date",prop:"date",rFormat:d.dateTime},{label:"Name",prop:"name"},{label:"Address",prop:"address"}],query(){return new Promise(n=>{setTimeout(()=>{const e=new Array(10).fill(0).map(()=>({date:new Date().toString(),name:"name",address:Math.random().toString()}));n({list:e,total:100})},1e3)})}});return o(r),{tableState:a,pagination:t}},template:`
         <div>当前已选择数：{{ tableState.selected.length }}</div>
         <Table v-bind="tableState" height=300 />
         <ElPagination v-bind="pagination" />
-      `})},m={render:()=>({components:{Table:l},setup(){const{tableState:a,handleQuery:r,table:t}=s({columns:[{label:"Date",prop:"date",rFormat:d.dateTime},{label:"Name",prop:"name"},{label:"Address",prop:"address"}],query(){return new Promise(e=>{setTimeout(()=>{const n=new Array(10).fill(0).map(()=>({date:new Date().toString(),name:"name",address:Math.random().toString()}));e({list:n,total:100})},1e3)})}});return o(r),o(()=>{R(()=>{console.log("组件实例：",t.value)})}),{tableState:a}},template:`
+      `})},p={render:()=>({components:{Table:l},setup(){const{tableState:a,handleQuery:r,table:t}=s({columns:[{label:"Date",prop:"date",rFormat:d.dateTime},{label:"Name",prop:"name"},{label:"Address",prop:"address"}],query(){return new Promise(n=>{setTimeout(()=>{const e=new Array(10).fill(0).map(()=>({date:new Date().toString(),name:"name",address:Math.random().toString()}));n({list:e,total:100})},1e3)})}});return o(r),o(()=>{Y(()=>{console.log("组件实例：",t.value)})}),{tableState:a}},template:`
         <Table v-bind="tableState" height=300 />
-      `})},g={render:()=>({components:{Table:l},setup(){const{tableState:a,handleQuery:r,toggleColumn:t}=s({columns:[{label:"Date",prop:"date",rFormat:d.dateTime},{label:"Name",prop:"name"},{label:"Address",prop:"address",formatter(e,n,h,F){return T("div",null,[S("addrrrrr")])}}],query(){return new Promise(e=>{setTimeout(()=>{const n=new Array(20).fill({date:new Date().toString(),name:"name",address:Math.random().toString()});e({list:n,total:100})},1e3)})}});return o(r),{tableState:a,toggleColumn:t}},template:`
+      `})},h={render:()=>({components:{Table:l},setup(){const{tableState:a,handleQuery:r,toggleColumn:t}=s({columns:[{label:"Date",prop:"date",rFormat:d.dateTime},{label:"Name",prop:"name"},{label:"Address",prop:"address",formatter(n,e,u,F){return c("div",null,[b("addrrrrr")])}}],query(){return new Promise(n=>{setTimeout(()=>{const e=new Array(20).fill({date:new Date().toString(),name:"name",address:Math.random().toString()});n({list:e,total:100})},1e3)})}});return o(r),{tableState:a,toggleColumn:t}},template:`
         <Table v-bind="tableState" height=300 />
         <button @click="() => toggleColumn('name', false)">隐藏Name列</button>
         <button @click="() => toggleColumn('name', true)">显示Name列</button>
         <button @click="() => toggleColumn('name')">显示/隐藏Name列</button>
-      `})};var E,A,v;p.parameters={...p.parameters,docs:{...(E=p.parameters)==null?void 0:E.docs,source:{originalSource:`{
+      `})},y={render:()=>({components:{Table:l},setup(){const{tableState:a,handleQuery:r,setState:t}=s({columns:[{label:"Date",prop:"date",rFormat:d.dateTime},{label:"Name",prop:"name"},{label:"Address",prop:"address",formatter(e,u,F,ee){return c("div",null,[b("addrrrrr")])}}],query(){return new Promise(e=>{setTimeout(()=>{const u=new Array(20).fill({date:new Date().toString(),name:"name",address:Math.random().toString()});e({list:u,total:100})},1e3)})}});return o(r),{tableState:a,changeColumns:()=>{t({columns:[{label:"Name",prop:"name",formatter:e=>e.name+"change"}]})}}},template:`
+        <Table v-bind="tableState" height=300 />
+        <button @click="changeColumns">改变columns</button>
+      `})};var D,A,E;g.parameters={...g.parameters,docs:{...(D=g.parameters)==null?void 0:D.docs,source:{originalSource:`{
   args: {
     height: 300,
     data: [{
@@ -50,7 +53,7 @@ https://element-plus.org/zh-CN/component/table.html`}}}},p={args:{height:300,dat
       prop: 'address'
     }]
   }
-}`,...(v=(A=p.parameters)==null?void 0:A.docs)==null?void 0:v.source}}};var w,D,_,f,N;u.parameters={...u.parameters,docs:{...(w=u.parameters)==null?void 0:w.docs,source:{originalSource:`{
+}`,...(E=(A=g.parameters)==null?void 0:A.docs)==null?void 0:E.source}}};var v,_,f,N,C;m.parameters={...m.parameters,docs:{...(v=m.parameters)==null?void 0:v.docs,source:{originalSource:`{
   render: () => {
     return {
       components: {
@@ -110,8 +113,8 @@ https://element-plus.org/zh-CN/component/table.html`}}}},p={args:{height:300,dat
       \`
     };
   }
-}`,...(_=(D=u.parameters)==null?void 0:D.docs)==null?void 0:_.source},description:{story:`配合useTable使用
-内部会自动生成符合OneTable组件props的响应式状态 同时返回一个符合el-pagination组件props的分页状态`,...(N=(f=u.parameters)==null?void 0:f.docs)==null?void 0:N.description}}};var C,M,P;c.parameters={...c.parameters,docs:{...(C=c.parameters)==null?void 0:C.docs,source:{originalSource:`{
+}`,...(f=(_=m.parameters)==null?void 0:_.docs)==null?void 0:f.source},description:{story:`配合useTable使用
+内部会自动生成符合OneTable组件props的响应式状态 同时返回一个符合el-pagination组件props的分页状态`,...(C=(N=m.parameters)==null?void 0:N.docs)==null?void 0:C.description}}};var M,P,Q;T.parameters={...T.parameters,docs:{...(M=T.parameters)==null?void 0:M.docs,source:{originalSource:`{
   render: () => {
     return {
       components: {
@@ -175,7 +178,7 @@ https://element-plus.org/zh-CN/component/table.html`}}}},p={args:{height:300,dat
       \`
     };
   }
-}`,...(P=(M=c.parameters)==null?void 0:M.docs)==null?void 0:P.source}}};var Q,x,B,V,q;i.parameters={...i.parameters,docs:{...(Q=i.parameters)==null?void 0:Q.docs,source:{originalSource:`{
+}`,...(Q=(P=T.parameters)==null?void 0:P.docs)==null?void 0:Q.source}}};var x,V,q,B,L;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:`{
   render: () => {
     return {
       components: {
@@ -232,7 +235,7 @@ https://element-plus.org/zh-CN/component/table.html`}}}},p={args:{height:300,dat
       \`
     };
   }
-}`,...(B=(x=i.parameters)==null?void 0:x.docs)==null?void 0:B.source},description:{story:"通过 `el-table-infinite-scroll` 实现 然后useTable代理onNext实现",...(q=(V=i.parameters)==null?void 0:V.docs)==null?void 0:q.description}}};var L,k,G;b.parameters={...b.parameters,docs:{...(L=b.parameters)==null?void 0:L.docs,source:{originalSource:`{
+}`,...(q=(V=i.parameters)==null?void 0:V.docs)==null?void 0:q.source},description:{story:"通过 `el-table-infinite-scroll` 实现 然后useTable代理onNext实现",...(L=(B=i.parameters)==null?void 0:B.docs)==null?void 0:L.description}}};var k,G,z;S.parameters={...S.parameters,docs:{...(k=S.parameters)==null?void 0:k.docs,source:{originalSource:`{
   render: () => {
     return {
       components: {
@@ -293,7 +296,7 @@ https://element-plus.org/zh-CN/component/table.html`}}}},p={args:{height:300,dat
       \`
     };
   }
-}`,...(G=(k=b.parameters)==null?void 0:k.docs)==null?void 0:G.source}}};var z,O,j,$,H;m.parameters={...m.parameters,docs:{...(z=m.parameters)==null?void 0:z.docs,source:{originalSource:`{
+}`,...(z=(G=S.parameters)==null?void 0:G.docs)==null?void 0:z.source}}};var O,j,$,H,I;p.parameters={...p.parameters,docs:{...(O=p.parameters)==null?void 0:O.docs,source:{originalSource:`{
   render: () => {
     return {
       components: {
@@ -352,7 +355,7 @@ https://element-plus.org/zh-CN/component/table.html`}}}},p={args:{height:300,dat
       \`
     };
   }
-}`,...(j=(O=m.parameters)==null?void 0:O.docs)==null?void 0:j.source},description:{story:"打开控制台查看",...(H=($=m.parameters)==null?void 0:$.docs)==null?void 0:H.description}}};var I,J,K;g.parameters={...g.parameters,docs:{...(I=g.parameters)==null?void 0:I.docs,source:{originalSource:`{
+}`,...($=(j=p.parameters)==null?void 0:j.docs)==null?void 0:$.source},description:{story:"打开控制台查看",...(I=(H=p.parameters)==null?void 0:H.docs)==null?void 0:I.description}}};var J,K,R;h.parameters={...h.parameters,docs:{...(J=h.parameters)==null?void 0:J.docs,source:{originalSource:`{
   render: () => {
     return {
       components: {
@@ -413,5 +416,73 @@ https://element-plus.org/zh-CN/component/table.html`}}}},p={args:{height:300,dat
       \`
     };
   }
-}`,...(K=(J=g.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};const ae=["基础用例","配合useTable使用","自定义分页参数","无限滚动列表","可选择列表","获取ElTable组件实例","可隐藏Table列"];export{ae as __namedExportsOrder,te as default,b as 可选择列表,g as 可隐藏Table列,p as 基础用例,i as 无限滚动列表,c as 自定义分页参数,m as 获取ElTable组件实例,u as 配合useTable使用};
-//# sourceMappingURL=Table.stories-039a3ac9.js.map
+}`,...(R=(K=h.parameters)==null?void 0:K.docs)==null?void 0:R.source}}};var U,W,X;y.parameters={...y.parameters,docs:{...(U=y.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  render: () => {
+    return {
+      components: {
+        Table
+      },
+      setup() {
+        type Data = {
+          date: string;
+          name: string;
+          address: string;
+        };
+        const {
+          tableState,
+          handleQuery,
+          setState
+        } = useTable<Data>({
+          columns: [{
+            label: 'Date',
+            prop: 'date',
+            rFormat: Format.dateTime
+          }, {
+            label: 'Name',
+            prop: 'name'
+          }, {
+            label: 'Address',
+            prop: 'address',
+            formatter(_row, _column, _cellValue, _index) {
+              return <div>addrrrrr</div>;
+            }
+          }],
+          query() {
+            return new Promise(r => {
+              setTimeout(() => {
+                const list = new Array(20).fill({
+                  date: new Date().toString(),
+                  name: 'name',
+                  address: Math.random().toString()
+                });
+                r({
+                  list,
+                  total: 100
+                });
+              }, 1000);
+            });
+          }
+        });
+        onMounted(handleQuery);
+        const changeColumns = () => {
+          setState({
+            columns: [{
+              label: 'Name',
+              prop: 'name',
+              formatter: row => row.name + 'change'
+            }]
+          });
+        };
+        return {
+          tableState,
+          changeColumns
+        };
+      },
+      template: \`
+        <Table v-bind="tableState" height=300 />
+        <button @click="changeColumns">改变columns</button>
+      \`
+    };
+  }
+}`,...(X=(W=y.parameters)==null?void 0:W.docs)==null?void 0:X.source}}};const de=["基础用例","配合useTable使用","自定义分页参数","无限滚动列表","可选择列表","获取ElTable组件实例","可隐藏Table列","重置Table列"];export{de as __namedExportsOrder,se as default,S as 可选择列表,h as 可隐藏Table列,g as 基础用例,i as 无限滚动列表,T as 自定义分页参数,p as 获取ElTable组件实例,m as 配合useTable使用,y as 重置Table列};
+//# sourceMappingURL=Table.stories-41c0a6de.js.map
--- assets/WithTooltip-FBT32F6Q-42affc9f.js
+++
@@ -1,2 +0,0 @@
-import{b as s,b as W,d as b}from"./DocsRenderer-7FRJXR4N-7c45d47d.js";import"./iframe-85414cba.js";import"../sb-preview/runtime.js";import"./index-c013ead5.js";import"./_commonjsHelpers-725317a4.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-e5b5e982.js";import"./index-356e4a49.js";import"./preview-6d879347.js";export{s as WithToolTipState,W as WithTooltip,b as WithTooltipPure};
-//# sourceMappingURL=WithTooltip-FBT32F6Q-42affc9f.js.map
